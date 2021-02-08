66ae32be6f3fb5814e2d40c069c6c12a6fe954d5
tidy-dev <tidy-dev@github.com>
2021-02-08 13:22:19 -0500

    Successful Multiple Commit Cherry Picking

--- app/test/unit/git/cherry-pick-test.ts
+++ app/test/unit/git/cherry-pick-test.ts
@@ -108,4 +108,64 @@ describe('git/cherry-pick', () => {
       expect(result).toBe(CherryPickResult.CompletedWithoutError)
     })
   })
+
+  describe('successful cherry pick multiple commit without conflicts', () => {
+    let firstCommitSha: string
+
+    beforeEach(async () => {
+      firstCommitSha = featureBranch.tip.sha
+
+      // add two more commits to cherry pick
+      await switchTo(repository, featureBranchName)
+
+      const featureBranchCommitTwo = {
+        commitMessage: 'Cherry Picked Feature! Number Two',
+        entries: [
+          {
+            path: 'THING_TWO.md',
+            contents: '# HELLO WORLD! \nTHINGS GO HERE\n',
+          },
+        ],
+      }
+
+      await makeCommit(repository, featureBranchCommitTwo)
+
+      const featureBranchCommitThree = {
+        commitMessage: 'Cherry Picked Feature! Number Three',
+        entries: [
+          {
+            path: 'THING_THREE.md',
+            contents: '# HELLO WORLD! \nTHINGS GO HERE\n',
+          },
+        ],
+      }
+
+      await makeCommit(repository, featureBranchCommitThree)
+
+      featureBranch = await getBranchOrError(repository, featureBranchName)
+
+      // switch to the target branch
+      await switchTo(repository, targetBranchName)
+
+      const commitRange = `${firstCommitSha}^..${featureBranch.tip.sha}`
+      result = await cherryPick(repository, commitRange)
+    })
+
+    it('the target branch has', async () => {
+      const cherryPickedCommit = await getCommit(
+        repository,
+        featureBranch.tip.sha
+      )
+      expect(cherryPickedCommit?.summary).toBe('foo')
+
+      const commits = await getCommits(repository, targetBranch.ref, 5)
+      // should be starter commit and feature branch commit
+      expect(commits.length).toBe(4)
+      expect(commits[0].summary).toBe(cherryPickedCommit!.summary)
+    })
+
+    it('the result is that it completed without error', async () => {
+      expect(result).toBe(CherryPickResult.CompletedWithoutError)
+    })
+  })
 })
